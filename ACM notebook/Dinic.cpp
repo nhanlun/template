eef1f75f80a89383381f7a9d841e323a4778c955
Nhan Lun <bebi10181@yahoo.com.vn>
2019-12-26 16:57:10 +0700

    Format code

--- ACM notebook/Dinic.cpp
+++ ACM notebook/Dinic.cpp
@@ -1,40 +1,46 @@
 bool BFS()
 {
 	queue<int> q;
-	for (int i=1; i<=n; i++) d[i]=0,Free[i]=true;
+	// reset array
+	memset(d, 0, sizeof(d));
+	memset(Free, true, sizeof(Free));
 	q.push(s);
-	d[s]=1;
+	d[s] = 1;
 	while (!q.empty())
 	{
-    	int u=q.front(); q.pop();
-    	for (int i=0; i<DSK[u].size(); i++)
-    	{
-        	int v=DSK[u][i].fi;
-        	if (d[v]==0 && DSK[u][i].se>f[u][v])
-        	{
-            	d[v]=d[u]+1;
-            	q.push(v);
-        	}
-    	}
+		int u = q.front();
+		q.pop();
+		for (int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].fi;
+			if (d[v] == 0 && adj[u][i].se > f[u][v])
+			{
+				d[v] = d[u] + 1;
+				q.push(v);
+			}
+		}
 	}
-	return d[t]!=0;
+	return d[t] != 0;
 }
- 
-int DFS(int x,int delta)
+
+int DFS(int x, int delta)
 {
-	if (x==t) return delta;
-	Free[x]=false;
-	for (int i=0; i<DSK[x].size(); i++)
+	if (x == t)
+		return delta;
+	Free[x] = false;
+	for (int i = 0; i < adj[x].size(); i++)
 	{
-    	int y=DSK[x][i].fi;
-    	if (d[y]==d[x]+1 && f[x][y]<DSK[x][i].se && Free[y])
-    	{
-        	int tmp=DFS(y,min(delta,DSK[x][i].se-f[x][y]));
-        	if (tmp>0)
-        	{
-            	f[x][y]+=tmp; f[y][x]-=tmp; return tmp;
-        	}
-    	}
+		int y = adj[x][i].fi;
+		if (d[y] == d[x] + 1 && f[x][y] < adj[x][i].se && Free[y])
+		{
+			int tmp = DFS(y, min(delta, adj[x][i].se - f[x][y]));
+			if (tmp > 0)
+			{
+				f[x][y] += tmp;
+				f[y][x] -= tmp;
+				return tmp;
+			}
+		}
 	}
 	return 0;
 }
