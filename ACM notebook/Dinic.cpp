109d1512910db7282fff32fa43727b15f613b534
Nhan Lun <bebi10181@yahoo.com.vn>
2020-01-11 17:18:55 +0700

    Updated Dinic

--- ACM notebook/Dinic.cpp
+++ ACM notebook/Dinic.cpp
@@ -1,46 +1,56 @@
-bool BFS()
+namespace Dinic // really fast
 {
-	queue<int> q;
-	// reset array
-	memset(d, 0, sizeof(d));
-	memset(Free, true, sizeof(Free));
-	q.push(s);
-	d[s] = 1;
-	while (!q.empty())
-	{
-		int u = q.front();
-		q.pop();
-		for (int i = 0; i < adj[u].size(); i++)
-		{
-			int v = adj[u][i].fi;
-			if (d[v] == 0 && adj[u][i].se > f[u][v])
-			{
-				d[v] = d[u] + 1;
-				q.push(v);
-			}
-		}
-	}
-	return d[t] != 0;
-}
-
-int DFS(int x, int delta)
-{
-	if (x == t)
-		return delta;
-	Free[x] = false;
-	for (int i = 0; i < adj[x].size(); i++)
-	{
-		int y = adj[x][i].fi;
-		if (d[y] == d[x] + 1 && f[x][y] < adj[x][i].se && Free[y])
-		{
-			int tmp = DFS(y, min(delta, adj[x][i].se - f[x][y]));
-			if (tmp > 0)
-			{
-				f[x][y] += tmp;
-				f[y][x] -= tmp;
-				return tmp;
-			}
-		}
-	}
-	return 0;
-}
+    vector<int> adj[N];
+    long long c[N][N], f[N][N];
+    int s = 0, t = 0, d[N], ptr[N];
+    bool BFS()
+    {
+        queue<int> q;
+        memset(d, -1, sizeof(d));
+        d[s] = 0; q.push(s);
+        while (!q.empty())
+        {
+            int u = q.front(); q.pop();
+            for (int v : adj[u])
+            {
+                if (d[v] == -1 && c[u][v] > f[u][v])
+                {
+                    d[v] = d[u] + 1;
+                    q.push(v);
+                }
+            }
+        }
+        return d[t] != -1;
+    }
+    long long DFS(int x, long long delta)
+    {
+        if (x == t) return delta;
+        for (; ptr[x] < adj[x].size(); ++ptr[x]) // Skip the used edge
+        {
+            int y = adj[x][ptr[x]];
+            if (d[y] == d[x] + 1 && c[x][y] > f[x][y])
+            {
+                long long push = DFS(y, min(delta, c[x][y] - f[x][y]));
+                if (push)
+                {
+                    f[x][y] += push;
+                    f[y][x] -= push;
+                    return push;
+                }
+            }
+        }
+        return 0;
+    }
+    long long maxFlow(int x, int y) // From x to y
+    {
+        long long flow = 0;
+        s = x; t = y;
+        while (BFS())
+        {
+            memset(ptr, 0, sizeof(ptr)); 
+            while (long long tmp = DFS(s, 1e9))
+                flow += 1LL * tmp;
+        }
+        return flow;
+    }
+};
