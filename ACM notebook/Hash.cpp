28cfc661ea4dda239af1f48afaca55f76820d38b
nhanlun <ntnhan18@apcs.vn>
2020-03-20 12:59:59 +0700

    Added Hash

---
+++ ACM notebook/Hash.cpp
@@ -0,0 +1,57 @@
+long long POW[Bases][N];
+
+struct Hash
+{
+  long long a[Bases];
+  Hash operator+(const Hash& src)
+  {
+    Hash tmp;
+    for (int i = 0; i < Bases; ++i) tmp.a[i] = addi(a[i], src.a[i]);
+    return tmp;
+  }
+  Hash operator-(const Hash& src)
+  {
+    Hash tmp;
+    for (int i = 0; i < Bases; ++i) tmp.a[i] = subt(a[i], src.a[i]);
+    return tmp;
+  }
+  Hash operator*(int x)
+  {
+    Hash tmp;
+    for (int i = 0; i < Bases; ++i) tmp.a[i] = mult(a[i], POW[i][x]);
+    return tmp;
+  }
+  Hash operator+(char c) 
+  { 
+    Hash tmp;
+    for (int i = 0; i < Bases; ++i) tmp.a[i] = addi(a[i], c); 
+    return tmp;
+  }
+  bool operator==(const Hash& src)
+  {
+    for (int i = 0; i < Bases; ++i) if (a[i] != src.a[i]) return false;
+    return true;
+  }
+};
+
+Hash hash1[N], hash2[N]; 
+void initHash(int n)
+{
+  for (int j = 0; j < Bases; ++j) POW[j][0] = 1;
+  for (int j = 0; j < Bases; ++j) for (int i = 1; i <= n; ++i) POW[j][i] = mult(POW[j][i - 1], base[j]);
+}
+
+void calcHash(int n)
+{
+  for (int j = 0; j < Bases; ++j) hash1[j].a[0] = 0;
+  for (int i = 1; i <= n; ++i) hash1[i] = hash1[i - 1] * 1 + (s[i] - 'a');
+}
+
+void calcHashRev(int n)
+{
+  for (int j = 0; j < Bases; ++j) hash2[j].a[n + 1] = 0;
+  for (int i = n; i >= 0; --i) hash2[i] = hash2[i + 1] * 1 + (s[i] - 'a');
+}
+
+Hash getHash(int l, int r) { return hash1[r] - hash1[l - 1] * (r - l + 1); }
+Hash getHashRev(int l, int r) { return hash2[l] - hash2[r + 1] * (r - l + 1); }
--- ACM notebook/define.cpp
+++ ACM notebook/define.cpp
@@ -3,13 +3,40 @@
 #include <chrono>
 #include <ctime>
 
-#define N
-#define matrix_size 2
-#define mod 1000000007LL
-#define eps 1e-8
-#define base 137 // Or 37
-
 #define cross(A, B) (A.x * B.y - A.y * B.x)
 #define dot(A, B) (A.x * B.x + A.y * B.y)
 #define ccw(A, B, C) (-(A.x * (C.y - B.y) + B.x * (A.y - C.y) + C.x * (B.y - A.y))) // positive when ccw
 #define CROSS(a, b, c, d) (a * d - b * c)
+
+#define fi first
+#define se second
+#define LL(x) (x << 1)
+#define RR(x) ((x << 1) + 1)
+#define mp make_pair
+ 
+using namespace std;
+const int N = 1000005;
+const int M = 30000;
+ 
+const int Bases = 2;
+const long long base[] = {137, 37};
+const long long mod = 1000000007LL;
+ 
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<int, iii> iiii;
+
+long long addi(long long a, long long b, long long m = mod) { a += b; if (a < 0) a += m; if (a >= m) a -= m; return a; }
+long long subt(long long a, long long b, long long m = mod) { a -= b; if (a < 0) a += m; if (a >= m) a -= m; return a; }
+long long mult(long long a, long long b, long long m = mod) { return a * b % m; }
+long long power(long long a, long long b, long long m = mod) 
+{
+    long long tmp = 1;
+    for (; b > 0; b >>= 1)
+    {
+        if (b & 1LL) tmp = mult(tmp, a, m);
+        a = mult(a, a, m);
+    }
+    return tmp;
+}
+long long inv(long long a, long long m = mod) { return power(a, m - 2, m); }
