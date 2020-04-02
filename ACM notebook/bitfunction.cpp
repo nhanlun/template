1e3b44aa2285eae21ee461b71856cf8c89efb0df
nhanlun <ntnhan18@apcs.vn>
2020-04-02 22:48:32 +0700

    Updated bitfunction

--- ACM notebook/bitfunction.cpp
+++ ACM notebook/bitfunction.cpp
@@ -8,3 +8,6 @@ __builtin_ctzll(x); // for long long
 (x & ~x) : the smallest bit 1 in x
 floor(log2(x)) : 31 - __builtin_clz(x | 1);
 floor(log2(x)) : 63 - __builtin_clzll(x | 1);
+
+int n = 1 << s;
+m & (n - 1); // m mod (1 << s)
