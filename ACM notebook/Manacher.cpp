066d08241e17da0f0b3412e9f7f4d997bdcf87dd
Nhân Nguyễn <ngthanhnhanlhp2000@gmail.com>
2021-12-15 22:25:09 +0700

    update manacher

--- ACM notebook/Manacher.cpp
+++ ACM notebook/Manacher.cpp
@@ -1,23 +1,26 @@
-void init() {
-  cnt = 0;
+string manacher(string s) {
+  // warning: use string can TLE, please use vector char
+  vector<char> t(s.length() * 2 + 5); 
+  int cnt = 0;
   t[0] = '~';
-  for (int i = 0; i<n; i++) {
-    t[++cnt] = '#';t[++cnt] = s[i];
+  for (int i = 0; i < s.length(); ++i) {
+    t[++cnt] = '#'; t[++cnt] = s[i];
   }
   t[++cnt] = '#'; t[++cnt] = '-';
-}
- 
-void manacher() {
-  int n = cnt - 2;
-  int r = 1; int C = 1;
-  int ans = 0;
-  for (int i = 2; i<n; i++) {
-    int i_mirror = C * 2 - i;
-    z[i] = (r > i) ? min(z[i_mirror], r - i) : 0;
-    while (t[i + z[i] + 1] == t[i - z[i] - 1]) z[i]++;
+
+  string res; int Max = 0;
+  vector<int> z(cnt, 0);
+  for (int i = 2, center = 1, r = 1; i < cnt - 2; ++i) {
+    int i_mirror = center * 2 - i;
+    z[i] = (r > i) ? min(r - i, z[i_mirror]) : 0;
+    while (t[i + z[i] + 1] == t[i - z[i] - 1]) ++z[i];
     if (i + z[i] > r) {
-      C = i;
       r = i + z[i];
+      center = i;
     }
+    if (z[i] > z[Max]) Max = i;
   }
+  for (int i = Max - z[Max] + 1; i <= Max + z[Max] - 1; i += 2) 
+    res += t[i];
+  return res;
 }
